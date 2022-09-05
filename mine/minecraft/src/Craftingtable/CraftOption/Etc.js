c71f4cbcae5dfbf26264f1903188f575bf780a6e
Ji Min Cha <jimin8830@naver.com>
2022-09-05 23:25:27 +0900

    [수정] Carousel

--- mine/minecraft/src/Craftingtable/CraftOption/Etc.js
+++ mine/minecraft/src/Craftingtable/CraftOption/Etc.js
@@ -24,23 +24,24 @@ const Etc = () =>{
 
     useEffect(()=>{
         const nextBtn = document.querySelector('.next-btn');
-        const container = document.querySelector('.wrap_frame');
+        const container = document.querySelector('.slides');
         const prevBtn = document.querySelector('.prev-btn');
         const slideLength = document.querySelectorAll('.card').length;
 
         let currentSlide = 1;
-        const IMAGE_WIDTH = 100;
+        const IMAGE_WIDTH = 350;
         // 100
 
         //next btn
         nextBtn.addEventListener('click', next);
 
         function next(){
-            console.log(currentSlide);
+            // console.log(currentSlide);
             if (currentSlide >= slideLength) {
                 currentSlide = 0;
               }
-            container.style.transform = `translateX(-${IMAGE_WIDTH * currentSlide}vw)`;
+            container.style.transform = `translateX(-${IMAGE_WIDTH * currentSlide}px)`;
+            // container.style.transform = `translateX(-300px)`;
             currentSlide++;
         }
 
@@ -52,37 +53,51 @@ const Etc = () =>{
             } else {
                 currentSlide--;
             }
-            container.style.transform = `translateX(-${IMAGE_WIDTH * (currentSlide - 1)}vw)`;
+            container.style.transform = `translateX(-${IMAGE_WIDTH * (currentSlide - 1)}px)`;
+            // container.style.transform = `translateX(300px)`;
         }
     })
     
     return(
         <>
-        <div >
-            <div className="container"> 
-                <div className="wrap_frame">
-                    <div className="test">
-                    <div className="frame"> 
-                        <div className="card">
-                            <h1>1</h1>
-                        </div>
-                    </div> </div>
+            <div className="slide_wrapper "> 
+                <div className="slides">
+                    <div className="wrap_frame">
+                        <div className="frame"> 
+                            <div className="card">
+                                <h1>1</h1>
+                            </div>
+                            
+                        </div> 
+                            
                         
-                    
-                    <div className="frame">
-                        <div className="card">
-                        <h1>2</h1>
+                        <div className="frame">
+                            <div className="card">
+                            <h1>2</h1>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="frame">
-                        <div className="card">
-                        <h1>3</h1>
+                        <div className="frame">
+                            <div className="card">
+                            <h1>3</h1>
+                            </div>
                         </div>
+
+                        <div className="frame">
+                            <div className="card">
+                            <h1>4</h1>
+                            </div>
+                        </div>
+
+                        <div className="frame">
+                            <div className="card">
+                            <h1>5</h1>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
 
         <button className="prev-btn" style={{height:"100px"}}>prev</button>
         <button className="next-btn" style={{height:"100px"}}>next</button>
